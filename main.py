ef578e50993521c11daf8ded38b10447774dcefd
MIMBOL228 <aekemerov@yandex.ru>
2023-03-18 19:16:00 +0500

    Big refactor and connect to website

--- main.py
+++ main.py
@@ -2,37 +2,36 @@
 Auto calls
 """
 import datetime
-import json
 import time
+import requests
+import os
 
+from dotenv import load_dotenv, find_dotenv
 import playsound
 
+load_dotenv(find_dotenv())
+
 mp3_files = ["1.mp3", "2.mp3"]
-auto_timer = []
-DEV_MODE = False
-JSON_NAME = 'times.json' if not DEV_MODE else 'times.dev.json'
-
+auto_timer = ""
 while True:
-    with open(JSON_NAME, 'r', encoding='utf8') as temp_file:
-        json_string = temp_file.read()
+    timeNow = datetime.datetime.now().strftime("%H:%M")
 
-    data = json.loads(json_string)
-    now = datetime.datetime.now()
-
-    if auto_timer != [now.hour, now.minute]:
-        auto_timer = []
-    first = [a for a in (data[datetime.datetime.today().weekday()] if not DEV_MODE else data)
-             if a[0][0] == now.hour and a[0] [1] == now.minute and a[0] != auto_timer]
-    second = [a for a in (data[datetime.datetime.today().weekday()] if not DEV_MODE else data)
-             if a[1][0] == now.hour and a[1] [1] == now.minute and a[1] != auto_timer]
-    if first:
-        auto_timer = first[0][0]
-        print("Звенит звонок на урок")
-        playsound.playsound(mp3_files[0], True)
-    if second:
-        auto_timer = second[0][1]
-        print("Звенит звонок с урока")
-        playsound.playsound(mp3_files[1], True)
-
+    if auto_timer != timeNow:
+        auto_timer = ""
+    if auto_timer == "":
+        response = requests.get(os.environ.get("API_URL") + "schedule/" + str(datetime.datetime.today().weekday()))
+        if not response:
+            print("API Error!!!")
+        response = response.json()
+        startLesson = [a for a in response if a['start'] == timeNow]
+        endLesson = [a for a in response if a['end'] == timeNow]
+        print(timeNow)
+        if len(startLesson) != 0:
+            auto_timer = timeNow
+            print("Звенит звонок на урок")
+            playsound.playsound(mp3_files[0], True)
+        elif len(endLesson) != 0:
+            auto_timer = timeNow
+            print("Звенит звонок с урока")
+            playsound.playsound(mp3_files[1], True)
     time.sleep(1)
-    
