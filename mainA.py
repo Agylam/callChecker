c36ae712993c3245e0c3b10ffdce7759d378aaf7
MIMBOL228 <aekemerov@yandex.ru>
2023-03-23 23:30:23 +0500

    Some changes

--- mainA.py
+++ mainA.py
@@ -5,34 +5,38 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-schedule=[] 
+schedule = []
+
 
 class Api:
     def __init__(self):
         self.api_url = os.environ.get("API_URL")
 
-    async def getSchedule(self, params=None):
+    async def get_schedule(self, params=None):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.url) as response:
                 return await response.json()
 
+
 apiObj = Api()
 
-async def startScheduleListener(dow):
+
+async def start_schedule_listener():
     while True:
-        data = await api_requester.get_data()
+        data = await apiObj.get_schedule()
         print(data)
         await asyncio.sleep(1)
 
-async def startCallsListener(dow):
+
+async def startCallsListener():
     while True:
-        data = await api_requester.get_data()
+        data = await apiObj.get_data()
         print(data)
         await asyncio.sleep(1)
 
 
 async def main():
-    
     await asyncio.gather(make_requests(api_requester1), make_requests(api_requester2))
+
 
 asyncio.run(main())
